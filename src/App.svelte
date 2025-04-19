2cc9de844d85699bff8ca5a5705af4a89f8eda26
Federico Melo <f.melo@uniandes.edu.co>
2025-04-19 17:56:12 -0500

    Use global CSS styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   import { HEADER } from './data/header';
   import Header from './components/Header.svelte';
+
+  import { RESEARCH_INTERESTS } from './data/researchInterests';
+  import ResearchInterests from './components/ResearchInterests.svelte';
+
+  import { EDUCATION } from './data/education';
+  import Education from './components/Education.svelte';
 </script>
 
 <main class="cv-container">
   <Header header={HEADER} />
+  <ResearchInterests researchInterests={RESEARCH_INTERESTS} />
+  <Education education={EDUCATION} />
 </main>
-
-<style>
-  .cv-container {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
-  }
-</style>
