f457a3dac005c707d8f587f847f958347aa4f3cd
Federico Melo <f.melo@uniandes.edu.co>
2025-04-19 17:16:35 -0500

    Set up svelte project

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { CV_DATA } from './data/cv-data';
+  import Header from './components/Header.svelte';
+</script>
+
+<main class="cv-container">
+  <Header name={CV_DATA.name} contact={CV_DATA.contact} />
+</main>
+
+<style>
+  .cv-container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+</style>
