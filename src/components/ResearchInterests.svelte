24b517f81ce4b7b3d4acfd528cdbb5bd91bea6cc
Federico Melo <f.melo@uniandes.edu.co>
2025-04-19 17:35:29 -0500

    Implement (dummy) research interests

---
+++ src/components/ResearchInterests.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let researchInterests: string;
+</script>
+
+<section>
+  <h2>Research Interests</h2>
+  <p>{researchInterests}</p>
+</section>
+
+<style>
+  @import '../styles/ResearchInterests.css';
+</style>
