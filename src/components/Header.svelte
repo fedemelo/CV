6651253bac117c517680d030e99eda9887fd1f19
Federico Melo <f.melo@uniandes.edu.co>
2025-04-19 22:56:34 -0500

    Rearrange contact information in headers

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,18 +1,28 @@
 <script lang="ts">
-  import type { Header } from '../data/header';
+  import type { Header } from "../data/header";
   export let header: Header;
 </script>
 
 <header>
   <h1>{header.name}</h1>
   <div class="contact-info">
-    <p>{header.email}</p>
-    <p>Webpage: <a href={header.webpage.url}>{header.webpage.label}</a></p>
-    <p>LinkedIn: <a href={header.linkedIn.url}>{header.linkedIn.label}</a></p>
-    <p>GitHub: <a href={header.github.url}>{header.github.label}</a></p>
+    <div class="columns-container">
+      <div class="column">
+        <p>
+          Email: <a href={`mailto:${header.email}`}>{header.email}</a>
+        </p>
+        <p>
+          LinkedIn: <a href={header.linkedIn.url}>{header.linkedIn.label}</a>
+        </p>
+      </div>
+      <div class="column">
+        <p>Webpage: <a href={header.webpage.url}>{header.webpage.label}</a></p>
+        <p>GitHub: <a href={header.github.url}>{header.github.label}</a></p>
+      </div>
+    </div>
   </div>
 </header>
 
 <style>
-  @import '../styles/Header.css';
+  @import "../styles/Header.css";
 </style>
