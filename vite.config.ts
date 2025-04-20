786dbed37f535ead5b90305893ef0d3249d75a1b
Federico Melo <f.melo@uniandes.edu.co>
2025-04-19 19:16:28 -0500

    Update vite.config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  base: '/cv/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
