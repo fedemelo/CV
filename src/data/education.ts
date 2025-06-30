729a025d699a9de1e81dd1c47493a967f8baebf8
Federico Melo <f.melo@uniandes.edu.co>
2025-06-29 21:48:17 -0500

    Update education

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,8 +1,9 @@
-import type { Hideable, MonthYear, Location} from "./types";
+import { UNIANDES } from "./constants";
+import type { Hideable, Location} from "./types";
 
 export interface Education extends Location, Hideable {
   degree: string;
-  institution: string;
+  organization: string;
   startDate?: Date;
   graduationDate?: Date;
   trueEndDate?: Date;  // Kept for historical reasons
@@ -15,12 +16,10 @@ export const EDUCATION: Education[] = [
   {
     degree: "B.Sc. Systems and Computing Engineering",
     honors: "Summa Cum Laude",
-    institution: "Universidad de los Andes",
-    city: "Bogotá",
-    country: "Colombia",
+    ...UNIANDES,
     startDate: new Date("2020-08-08"),  // Unclear if this is the actual start date
     graduationDate: new Date("2025-04-07"),
-    trueEndDate: new Date("2024-12-09"),  // Last day of classes
+    trueEndDate: new Date("2024-12-08"),  // Sunday, last day to turn in deliverables
     gpa: "4.92/5.00",
     details: [
       "Best cumulative GPA in the Systems and Computing Engineering program in the XXI century",
@@ -29,7 +28,7 @@ export const EDUCATION: Education[] = [
   },
   {
     degree: "Bachiller Académico",
-    institution: "Colegio San Carlos",
+    organization: "Colegio San Carlos",
     city: "Bogotá",
     country: "Colombia",
     graduationDate: new Date("2020-06-26"),
