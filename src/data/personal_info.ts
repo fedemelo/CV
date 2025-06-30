b5561ca527e1a2ee7c607ac4eaeb6a8e495ce466
Federico Melo <f.melo@uniandes.edu.co>
2025-06-29 21:47:08 -0500

    Rename header to personal info

--- src/data/personal_info.ts
+++ src/data/personal_info.ts
@@ -1,4 +1,4 @@
-export interface Header {
+export interface PersonalInfo {
   name: string;
   email: `${string}@${string}.${string}`;
   webpage: string;
@@ -6,7 +6,7 @@ export interface Header {
   github: string;
 }
 
-export const HEADER: Header = {
+export const PERSONAL_INFO: PersonalInfo = {
   name: "Federico Melo Barrero",
   email: "f.melo@uniandes.edu.co",
   webpage: "fedemelo.github.io",
