c4f68ade94d4b7b9072d76541127c31259965e27
Federico Melo <f.melo@uniandes.edu.co>
2025-07-10 22:49:41 -0500

    Show things by default

--- src/utils/show.ts
+++ src/utils/show.ts
@@ -1,7 +1,7 @@
 export function filterForCV<T extends { showInCV?: boolean }>(items: T[]) {
-  return items.filter((item) => item.showInCV);
+  return items.filter((item) => item.showInCV ?? true);
 }
 
 export function filterForResume<T extends { showInResume?: boolean }>(items: T[]) {
-  return items.filter((item) => item.showInResume);
+  return items.filter((item) => item.showInResume ?? true);
 }
