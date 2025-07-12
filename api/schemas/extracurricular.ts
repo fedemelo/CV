bc04db70cb0c82063bfefb4550c6e74783ac14a8
Federico Melo <f.melo@uniandes.edu.co>
2025-07-12 14:41:49 -0500

    Remove location from extracurriculars

--- api/schemas/extracurricular.ts
+++ api/schemas/extracurricular.ts
@@ -1,12 +1,10 @@
 import { z } from "zod";
 import { HideableSchema } from "./hideable";
-import { LocationSchema } from "./location";
 
 export type Extracurricular = z.infer<typeof ExtracurricularSchema>;
 
 export const ExtracurricularSchema = z.object({
   ...HideableSchema.shape,
-  ...LocationSchema.shape,
   description: z.string(),
   events: z.array(z.string()),
 });
