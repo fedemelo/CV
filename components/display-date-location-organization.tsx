94bc0aad8a24841fd9143d909503fa92612856eb
Federico Melo <f.melo@uniandes.edu.co>
2025-07-12 15:56:36 -0500

    Implement DisplayDateLocationOrganization

---
+++ components/display-date-location-organization.tsx
@@ -0,0 +1,26 @@
+import { Calendar, MapPin, Building2 } from "lucide-react"
+import type { Location } from "@/types"
+
+export function DisplayDateLocationOrganization({ date, location, organization }: { date?: string | number, location?: Location, organization?: string }) {
+  return (
+    <div className="flex flex-wrap items-center gap-4 text-sm text-muted-foreground">
+      {date && <div className="flex items-center">
+        <Calendar className="mr-1 h-4 w-4" />
+        {date}
+      </div>
+      }
+      {(location?.city || location?.state || location?.country) && <div className="flex items-center">
+        <MapPin className="mr-1 h-4 w-4" />
+        {location?.city && `${location.city}, `}
+        {location?.state && `${location.state}, `}
+        {location?.country && `${location.country}`}
+      </div>
+      }
+      {organization && <div className="flex items-center">
+        <Building2 className="mr-1 h-4 w-4" />
+        {organization}
+      </div>
+      }
+    </div>
+  )
+}
