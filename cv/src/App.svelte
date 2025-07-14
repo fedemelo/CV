d16e6b376c8e8d25d737f999d84bd224df2c72d4
Federico Melo <f.melo@uniandes.edu.co>
2025-07-13 20:12:02 -0500

    Make CV an independent workspace

---
+++ cv/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import CV from './CV.svelte';
+  import PrintButton from '../../shared/components/PrintButton.svelte';
+  import Footer from '../../shared/components/Footer.svelte';
+</script>
+
+<div class="app-container">
+  <div class="print-container">
+    <CV />
+  </div>
+  <Footer />
+  <PrintButton currentView="cv" />
+</div>
+
+<style>
+  .app-container {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+</style> 
