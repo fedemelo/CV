fdf2a41a34fd2c669c09c89c20622a91927491be
Federico Melo <f.melo@uniandes.edu.co>
2025-07-13 19:46:38 -0500

    Move some missing things to shared

--- shared/utils/show.ts
+++ shared/utils/show.ts
@@ -1,3 +1,6 @@
+export function filterForCV<T extends { showInCV?: boolean }>(items: T[]) {
+  return items.filter((item) => item.showInCV ?? true);
+}
 
 export function filterForResume<T extends { showInResume?: boolean }>(items: T[]) {
   return items.filter((item) => item.showInResume ?? true);
