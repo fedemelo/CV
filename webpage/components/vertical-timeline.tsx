96000f90ead3aeac87d6bb67aabad26b23afc2ca
Federico Melo <f.melo@uniandes.edu.co>
2025-07-15 22:23:20 -0500

    Center vertical timeline marks with cards

--- webpage/components/vertical-timeline.tsx
+++ webpage/components/vertical-timeline.tsx
@@ -7,8 +7,8 @@ interface VerticalTimelineProps {
 export function VerticalTimeline({ children }: VerticalTimelineProps) {
   return (
     <div className="relative">
-      <div className="absolute left-6 top-8 bottom-8 w-0.5 bg-border"></div>
-      <div className="space-y-8">
+      <div className="absolute left-5 top-10 bottom-10 w-0.5 bg-border"></div>
+      <div className="space-y-4">
         {children}
       </div>
     </div>
@@ -17,14 +17,14 @@ export function VerticalTimeline({ children }: VerticalTimelineProps) {
 
 export function TimelineItem({ children }: { children: ReactNode }) {
   return (
-    <div className="relative flex items-start">
+    <div className="relative flex items-center">
       <div className="relative z-10 flex-shrink-0">
-        <div className="w-12 h-12 bg-background border-4 border-primary rounded-full flex items-center justify-center">
+        <div className="w-10 h-10 bg-background border-4 border-primary rounded-full flex items-center justify-center">
           <div className="w-3 h-3 bg-primary rounded-full"></div>
         </div>
       </div>
       
-      <div className="ml-6 flex-1 pb-8">
+      <div className="ml-2 flex-1 pb-8">
         {children}
       </div>
     </div>
