ec06c6e43db7bbda1a2d5dba0c4c0e58fbcb28dd
Federico Melo <f.melo@uniandes.edu.co>
2025-07-17 20:32:21 -0500

    Adjust webpage in personal info data

--- shared/data/personalInfo.ts
+++ shared/data/personalInfo.ts
@@ -3,7 +3,7 @@ import type { PersonalInfo } from "../schemas/personalInfo";
 export const PERSONAL_INFO: PersonalInfo = {
   name: "Federico Melo Barrero",
   email: "f.melo@uniandes.edu.co",
-  webpage: "https://fedemelo.com",
+  webpage: "fedemelo.com",
   linkedInPath: "federico-melo",
   gitHubPath: "fedemelo",
 }
