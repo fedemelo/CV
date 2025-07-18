cd23450e50c512615acbdac84ddc785fa62eba20
Federico Melo <f.melo@uniandes.edu.co>
2025-07-17 21:16:46 -0500

    Add phone number to personal info

--- shared/data/personalInfo.ts
+++ shared/data/personalInfo.ts
@@ -2,6 +2,7 @@ import type { PersonalInfo } from "../schemas/personalInfo";
 
 export const PERSONAL_INFO: PersonalInfo = {
   name: "Federico Melo Barrero",
+  phone: "+57 310 202-2572",
   email: "f.melo@uniandes.edu.co",
   webpage: "fedemelo.com",
   linkedInPath: "federico-melo",
--- shared/schemas/personalInfo.ts
+++ shared/schemas/personalInfo.ts
@@ -4,6 +4,7 @@ export type PersonalInfo = z.infer<typeof PersonalInfoSchema>;
 
 export const PersonalInfoSchema = z.object({
   name: z.string(),
+  phone: z.string().optional(),
   email: z.string().email(),
   webpage: z.string().url(),
   linkedInPath: z.string(),
